4da5e2d088b8e3498061646e4d2b65aaade8cfd8
Evanna Wong <evannawong92@gmail.com>
2015-10-23 00:32:56 -0700

    Added quote addition functionality.

---
+++ application/views/quote_edit.php
@@ -0,0 +1,12 @@
+<!-- Form for maintaining a single quote -->
+
+<div class="row">
+    <div class="errors">{message}</div>
+    <form action="/admin/confirm" method="post">
+        {fid}
+        {fwho}
+        {fmug}
+        {fwhat}
+        {fsubmit}
+    </form>
+</div>
